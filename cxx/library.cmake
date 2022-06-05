b837868e471da70de956d764c5f2a9b4e354a615
Sven Suursoho <sven@alt.ee>
2022-06-05 16:35:53 +0300

    cxx_library: support for LIBRARIES

--- cxx/library.cmake
+++ cxx/library.cmake
@@ -1,5 +1,5 @@
 macro(cxx_library name)
-	cmake_parse_arguments(${name} "" "" "SOURCES" ${ARGN})
+	cmake_parse_arguments(${name} "" "" "SOURCES;LIBRARIES" ${ARGN})
 
 	message(CHECK_START "cxx_library(${name})")
 	list(APPEND CMAKE_MESSAGE_INDENT "    ")
@@ -12,6 +12,7 @@ macro(cxx_library name)
 			$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
 	)
 	target_compile_options(${name} PRIVATE ${cxx_max_warning_flags})
+	target_link_libraries(${name} ${${name}_LIBRARIES})
 
 	list(POP_BACK CMAKE_MESSAGE_INDENT)
 	message(CHECK_PASS "done")
