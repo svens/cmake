e835ee68f6489b4a5639db1e6794fd23cd1c33df
Sven Suursoho <sven@alt.ee>
2023-07-22 21:49:52 +0300

    cxx/msvc: handle /W flag setting with different cmake versions

--- cxx/msvc.cmake
+++ cxx/msvc.cmake
@@ -1,3 +1,7 @@
 # https://docs.microsoft.com/en-us/cpp/preprocessor/compiler-warnings-that-are-off-by-default
 set(cxx_max_warning_flags /W4 /WX /w34265 /w34777 /w34946 /w35038)
-string(REGEX REPLACE "/W3" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+
+if(${CMAKE_MINIMUM_REQUIRED_VERSION} VERSION_LESS "3.15")
+	# cmake >= 3.15 stopped adding /W3 option
+	string(REGEX REPLACE "/W3" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+endif()
